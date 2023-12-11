68e10b6559574aa8757780bf139275ca6fd3c219
kota-karthik <kotakarthik2307@gmail.com>
2023-12-11 21:50:10 +0530

    made whychooseus page responsive

--- diagnoseWizard/src/Pages/WhyChooseUs.jsx
+++ diagnoseWizard/src/Pages/WhyChooseUs.jsx
@@ -34,16 +34,16 @@ const WhyChooseUs = (props) => {
         ])
     const [array, setArray] = useState(["Wellness", "Compassion", "Quality", " Strength", "Fitness", "Resilience", " Energy",]);
     return (
-        <section className="my-[100px]" id="WhyChooseUs" ref={props.WhyChooseUsSection}>
-            <div>
-                <div className="flex justify-between w-[1073px] px-[75px] ">
+        <section className="my-[100px] text-start" id="WhyChooseUs" ref={props.WhyChooseUsSection}>
+            <div >
+                <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 lg:flex lg:justify-between w-[1073px] px-[75px] ">
                     <div className="inline-block translate-y-[-128px]">
                         <img src="assets/image 36.svg" alt="" className="z-30 translate-y-[128px] ml-[82px] relative" />
 
                         <div className="bg-[#FFC567] w-[385px] h-[128px] rounded-xl "> </div>
                     </div>
                     <div>
-                        <h3 className="font-bold text-[40px] mb-[30px]">
+                        <h3 className="text-start font-bold text-[40px] mb-[30px]">
                             Why Choose Us?
                         </h3>
                         {
@@ -59,14 +59,14 @@ const WhyChooseUs = (props) => {
                         }
                     </div>
                 </div>
-                <div className="w-[1120px] text-start px-[75px]">
+                <div className="flex-col lg:w-[1120px] text-start px-[75px]">
                     <h3 className="font-bold text-[40px] text-start mb-[30px]">
                         <TypeWriter array={array} />
                     </h3>
                     <p className="font-medium text-[20px] text-start mb-[45px]">
                         They live in Bookmarks grove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it.
                     </p>
-                    <button className="bg-[#00A0AA] font-bold text-[20px] text-[#FFFFFF] p-[15px] rounded-xl" onClick={navigateToHospitals}>
+                    <button className="ml-[0px] mt-[20px] bg-[#00A0AA] rounded-xl text-[15px] sm:text-[20px] font-bold text-center p-[15px] text-[#FFF] " onClick={navigateToHospitals}>
                         Take An Appointment
                     </button>
                 </div>
