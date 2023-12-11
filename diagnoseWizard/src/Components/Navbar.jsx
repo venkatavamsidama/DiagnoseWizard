c3ed7ee63cf98507161be5277237081e2a5fe938
kota-karthik <kotakarthik2307@gmail.com>
2023-12-11 23:58:46 +0530

    corrected a bug

--- diagnoseWizard/src/Components/Navbar.jsx
+++ diagnoseWizard/src/Components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = (props) => {
 
     return (
         <div>
-            <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <nav className="bg-white border-gray-200 ">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="#" className="flex mr-[80px]">
                         <img src="/assets/healthcare 1.svg" alt="" />
@@ -42,7 +42,7 @@ const Navbar = (props) => {
                     <button
                         onClick={toggleMenu}
                         type="button"
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
                         aria-controls="navbar-default"
                         aria-expanded="false"
                     >
